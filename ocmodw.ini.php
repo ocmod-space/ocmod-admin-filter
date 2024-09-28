b99c56b87b9edcb8ea040c09c97585e6f908997a
akaunderr <akaunderr@gmail.com>
2024-09-28 17:31:08 +0300

    .

---
+++ ocmodw.ini.php
@@ -0,0 +1,14 @@
+<?php
+
+define('VERSION', '2.4.0');
+define('YEAR', '2019');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://www.ocmod.space/admin-filter');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&filter_member=ocmod.space');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-admin-filter');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&amp;extension_id=36080');
+
+define('LICENSE', 'https://raw.githubusercontent.com/ocmod-space/ocmod-admin-filter/main/EULA.txt');
